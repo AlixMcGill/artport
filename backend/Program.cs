48651fd89a96c599928e12264af089113f25d056
AlixMcGill <alixfmcgill@gmail.com>
2026-03-27 22:23:31 -0500

    Added database connection

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,10 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("secrets.json", optional: false, reloadOnChange: true);
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseNpgsql(connectionString)
+    );
 
 builder.Services.AddOpenApi();
 
@@ -18,6 +24,24 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+}
+
+// Check database connection
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    try
+    {
+        var canConnect = db.Database.CanConnect();
+        Console.WriteLine(canConnect ? "Database Connection successful." :
+        "[WARNING] Database connection failed.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("[ERROR] Database Connection Failed During Startup.");
+        Console.WriteLine(ex.ToString());
+        throw;
+    }
 }
 
 app.UseHttpsRedirection();
--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae10b564fc39d63fbaec965e7ae30bd17517077b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ef34f61f6de7794353444fc6f3821634e3f155")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
